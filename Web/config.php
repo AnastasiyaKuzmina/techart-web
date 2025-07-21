936992910449b56f0badf58d320f98eb27dca811
AnastasiyaKuzmina <anastasiya.kuzmina.0202@mail.ru>
2025-07-21 12:38:37 +0300

    Add config.php

---
+++ Web/config.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'localhost';
+$user_name = 'anastle1_news';
+$password = 'HPKU%6OnaBmx';
+$db = 'anastle1_news';
+?>
--- Web/db.php
+++ Web/db.php
@@ -1,5 +1,6 @@
 <?php
-$conn = mysqli_connect('localhost', 'anastle1_news', 'HPKU%6OnaBmx', 'anastle1_news');
+require "config.php";
+$conn = mysqli_connect($host, $user_name, $password, $db);
 
 if (!$conn) {
     die('Ошибка подключения: ' . mysqli_error());
